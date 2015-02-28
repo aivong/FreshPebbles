32e446eca26c855be7cdac3704a40f199b228ded
aivong <aivong@users.noreply.github.com>
2015-02-28 06:55:59 -0600

    Add action for 'Box Office'
    Included test movie Card

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ splashWindow.add(text);
 splashWindow.show();
 
 // Create an array of Menu items
-var menuItems = [
+var mainMenuItems = [
   {title: "Box Office", subtitle: ""}, 
   {title: "Opening Movies", subtitle: ""}, 
   {title: "Upcoming Movies", subtitle: ""}
@@ -31,10 +31,24 @@ var menuItems = [
 var mainMenu = new UI.Menu({
   sections: [{
     title: 'Fresh Pebbles',
-    items: menuItems
+    items: mainMenuItems
   }]
 });
 
 // Show the Menu, hide the splash
 mainMenu.show();
 splashWindow.hide();
+
+// Add an action for 'Box Office'
+mainMenu.on('select', function(e) {
+  if(e.itemIndex == 0) {
+    var content = "test";
+    // Create the Card for a selected movie 
+    var movieCard = new UI.Card({
+      title:'Movie Details',
+      subtitle:e.item.subtitle,
+      body: content
+    });
+    movieCard.show();
+  }
+});
