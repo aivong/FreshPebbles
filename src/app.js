cfa25579b76d5c4b893774ac25cfe80076104b81
aivong <aivong@users.noreply.github.com>
2015-02-28 09:48:03 -0600

    Populated Opening Movie and Upcoming Movie lists

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ splashWindow.hide();
 // Add actions for main menu items
 mainMenu.on('select', function(e) {
   //'Box Office' movies selected
-  if(e.itemIndex == 0) {
+  if (e.itemIndex == 0) {
     // Make request to api.rottentomatoes.com for Box Office movies
     ajax(
       {
@@ -73,6 +73,139 @@ mainMenu.on('select', function(e) {
         
         // Add action for selected 'Box Office' movie
         boxOfficeMenu.on('select', function(e) {
+            // Make request to api.rottentomatoes.com for selected movie
+            ajax(
+            {
+              url:'http://api.rottentomatoes.com/api/public/v1.0/movies/' + e.item.subtitle + '.json?apikey=3u9s7zwwta4u97p2q3fp7t6x',
+              type:'json'
+            },
+            function(data) {
+              var title = data.title;
+              var year = data.year;
+              var synopsis = data.synopsis;
+              var criticScore = data.ratings.critics_score;
+              var audienceScore = data.ratings.audience_score;
+              var content = "Critic Score: " + criticScore; 
+              content += "\nAudience Score: ";
+              content += audienceScore;
+              content += "\n" + synopsis;
+              // Create detail Card for a selected movie 
+              var movieCard = new UI.Card({
+                 title: title,
+                 subtitle: year,
+                 body: content,
+                 scrollable: true
+               });
+              movieCard.show();    
+            },
+            function(error) {
+              console.log('Error: ' + error);
+            }
+          );
+        });
+      },
+      function(error) {
+        console.log('Error: ' + error);
+      }
+    );
+  }
+  else if (e.itemIndex == 1) {
+    // Make request to api.rottentomatoes.com for Box Office movies
+    ajax(
+      {
+        url:'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?limit=20&country=us&apikey=3u9s7zwwta4u97p2q3fp7t6x',
+        type:'json'
+      },
+      function(data) {
+        var openingMovieItems = [];
+        //fill Opening Movies list
+        for(var i = 0; i < 20; i++) {
+          var title = data.movies[i].title;
+          var id = data.movies[i].id;
+          openingMovieItems.push({
+            title: title,
+            subtitle: id,
+          });
+        }
+        
+        // Construct opening movie menu to show to user
+        var openingMovieMenu = new UI.Menu({
+          sections: [{
+            title: 'Opening Movies',
+            items: openingMovieItems
+          }]
+        });
+        openingMovieMenu.show();  
+        
+        // Add action for selected 'Opening' movie
+        openingMovieMenu.on('select', function(e) {
+            // Make request to api.rottentomatoes.com for selected movie
+            ajax(
+            {
+              url:'http://api.rottentomatoes.com/api/public/v1.0/movies/' + e.item.subtitle + '.json?apikey=3u9s7zwwta4u97p2q3fp7t6x',
+              type:'json'
+            },
+            function(data) {
+              var title = data.title;
+              var year = data.year;
+              var synopsis = data.synopsis;
+              var criticScore = data.ratings.critics_score;
+              var audienceScore = data.ratings.audience_score;
+              var content = "Critic Score: " + criticScore; 
+              content += "\nAudience Score: ";
+              content += audienceScore;
+              content += "\n" + synopsis;
+              // Create detail Card for a selected movie 
+              var movieCard = new UI.Card({
+                 title: title,
+                 subtitle: year,
+                 body: content,
+                 scrollable: true
+               });
+              movieCard.show();    
+            },
+            function(error) {
+              console.log('Error: ' + error);
+            }
+          );
+        });
+      },
+      function(error) {
+        console.log('Error: ' + error);
+      }
+    );
+  }
+ else if (e.itemIndex == 2) {
+    // Make request to api.rottentomatoes.com for Upcoming movies
+    ajax(
+      {
+        url:'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?page_limit=20&page=1&country=us&apikey=3u9s7zwwta4u97p2q3fp7t6x',
+        type:'json'
+      },
+      function(data) {
+        var total = data.total;
+        var upcomingMovieItems = [];
+        //fill Upcoming Movies list
+        for(var i = 0; i < data.total; i++) {
+          var title = data.movies[i].title;
+          var id = data.movies[i].id;
+          upcomingMovieItems.push({
+            title: title,
+            subtitle: id,
+          });
+        }
+        
+        // Construct upcoming movie menu to show to user
+        var upcomingMovieMenu = new UI.Menu({
+          sections: [{
+            title: 'Upcoming Movies',
+            items: upcomingMovieItems
+          }]
+        });
+        upcomingMovieMenu.show();  
+        
+        // Add action for selected 'Upcoming' movie
+        upcomingMovieMenu.on('select', function(e) {
             // Make request to api.rottentomatoes.com for selected movie
             ajax(
             {
